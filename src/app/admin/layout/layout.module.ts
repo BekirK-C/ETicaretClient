d9d0d695d76180ff01978b98882e80d7763a5797
BekirK-C <b.b.kamac@gmail.com>
2023-01-05 16:59:34 +0300

    11. Multiple Layout Yapılanmasının temelleri

--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { ComponentsModule } from './components/components.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +12,8 @@ import { ComponentsModule } from './components/components.module';
   ],
   imports: [
     CommonModule,
-    ComponentsModule //Oluşturulan module başka bir module'ü kendi içinde barındırıyorsa o module'ü import etmesi gerekir.
+    ComponentsModule, //Oluşturulan module başka bir module'ü kendi içinde barındırıyorsa o module'ü import etmesi gerekir.,
+    RouterModule
   ],
   exports:[
     LayoutComponent //export ederek dışarıdan erişilebilir olduğunu bildiriyorum.
